e0b645ed38f36b31ac2541a47179889a74c0756f
richard.vlas <vlasrichard@gmail.com>
2023-05-02 20:58:24 +0200

    challenge 1 done

---
+++ src/model/train.py
@@ -0,0 +1,82 @@
+# Import libraries
+
+import argparse
+import glob
+import mlflow
+import os
+import pandas as pd
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+
+# define functions
+def main(args):
+    # enable autologging - use mlflow autolog function
+    mlflow.autolog()
+
+    # read data
+    df = get_csvs_df(args.training_data)
+
+    # split data
+    X_train, X_test, y_train, y_test = split_data(df)
+
+    # train model
+    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+
+
+def get_csvs_df(path):
+    print(f"Reading CSVs from {path}")
+    if not os.path.exists(path):
+        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
+    csv_files = glob.glob(f"{path}/*.csv")
+    if not csv_files:
+        raise RuntimeError(f"No CSV files found in provided data path: {path}")
+    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
+
+
+# add function to split data
+def split_data(df):
+    # split data
+    X_train, X_test, y_train, y_test = train_test_split(df.drop("Diabetic", axis=1), df["Diabetic"], test_size=0.3, random_state=0)
+
+    # return data
+    return X_train, X_test, y_train, y_test
+
+
+def train_model(reg_rate, X_train, X_test, y_train, y_test):
+    # train model
+    LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+
+
+def parse_args():
+    # setup arg parser
+    parser = argparse.ArgumentParser()
+
+    # add arguments
+    parser.add_argument("--training_data", dest='training_data',
+                        type=str)
+    parser.add_argument("--reg_rate", dest='reg_rate',
+                        type=float, default=0.01)
+
+    # parse args
+    args = parser.parse_args()
+
+    # return args
+    return args
+
+# run script
+if __name__ == "__main__":
+    # add space in logs
+    print("\n\n")
+    print("*" * 60)
+
+    # parse args
+    args = parse_args()
+
+    # run main function
+    main(args)
+
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
